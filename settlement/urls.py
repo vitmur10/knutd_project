949c5efc81702dade28152684ff860a3d81a055b
Vitaliy Murovaniy <95306614+vitmur10@users.noreply.github.com>
2023-10-09 18:49:31 +0300

    головна

--- settlement/urls.py
+++ settlement/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'settlement'
 urlpatterns = [
+    # path('<int:hostel_number>/', views.hostel, name='hostel'),
+    # path('<int:hostel_number/floor_number>', views.floor, name='floor'),
     path('add_student/', views.add_student, name='add_student'),
     path('', views.dormitories, name='dormitories'),
     path('hostel/', views.hostel, name='hostel')
